43e8c0e7c13c99f6ec8d4b372078626abab1e7e6
Mathew Sunday <sunnycity2007@yahoo.com>
2023-08-29 02:30:31 -0700

    0x06-python-classes

---
+++ 0x06-python-classes/102-main.py
@@ -0,0 +1,15 @@
+s_5 = Square(5)
+s_6 = Square(6)
+
+if s_5 < s_6:
+    print("Square 5 < Square 6")
+if s_5 <= s_6:
+    print("Square 5 <= Square 6")
+if s_5 == s_6:
+    print("Square 5 == Square 6")
+if s_5 != s_6:
+    print("Square 5 != Square 6")
+if s_5 > s_6:
+    print("Square 5 > Square 6")
+if s_5 >= s_6:
+    print("Square 5 >= Square 6")
